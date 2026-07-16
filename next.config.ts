b76c84cf050bd33d632374ef4d3700fbd144dfd1
thanks-cohn <officialmahasbiz@gmail.com>
2026-07-16 16:22:33 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGitHubPages ? "/rotunda-galleries" : "",
+  assetPrefix: isGitHubPages ? "/rotunda-galleries/" : "",
 };
 
 export default nextConfig;
